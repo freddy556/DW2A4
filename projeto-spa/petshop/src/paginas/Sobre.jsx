1e0a365e9a76340f1afabd4090b14e90d01951f2
Lucas Mendes <lucas.mendes556@gmail.com>
2021-12-09 17:23:48 -0300

    Projeto finalizado

---
+++ projeto-spa/petshop/src/paginas/Sobre.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+import '../assets/css/componentes/cartao.css'
+
+const Sobre = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Sobre Doguito</h2>
+      </div>
+      <section className="container flex flex--centro">
+        <article className="cartao">
+          O blog do doguito foi criado para o projeto em SPA da matéria DW2A4!!!
+          Cabacinho
+        </article>
+        
+      </section>
+    </main>
+  )
+}
+
+export default Sobre
